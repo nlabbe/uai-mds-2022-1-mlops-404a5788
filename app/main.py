144b9f8e2676484661aa328cc1a5e9307eb03744
Nicolás Labbé <nlabbea@gmail.com>
2024-02-24 19:43:29 -0300

    Add exception handling to foo endpoint logic.

--- app/main.py
+++ app/main.py
@@ -14,21 +14,31 @@ def read_root():
 
 @app.get("/foo/")
 def read_pkl(sepalLength: float, sepalWidth: float, petalLength: float, petalWidth: float):
+
     url = 'https://uai-nico.s3.amazonaws.com/tree_iris_test.pkl'
     save_path = 'model/tree_iris_test.pkl'
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as f:
-            f.write(response.content)
-        
-        model = joblib.load('model/tree_iris_test.pkl')
-        arguments = np.array([[sepalLength, sepalWidth, petalLength, petalWidth]])
-        result = model.predict(arguments)
-        
-        print("File downloaded successfully")
-    else:
-        print("Failed to download file. Status code:", response.status_code)
 
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+            
+            model = joblib.load('model/tree_iris_test.pkl')
+            arguments = np.array([[sepalLength, sepalWidth, petalLength, petalWidth]])
+            result = model.predict(arguments)
+            
+            print("File downloaded successfully")
+        else:
+            print("Failed to download file. Status code:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
+    except IOError as e:
+        print("An error occurred while writing the file:", e)
+    except FileNotFoundError:
+        print("Error: The model file does not exist.")
+    except Exception as e:
+        print("An error occurred while loading the model:", e)
     
     return {"result": result[0]}
 
